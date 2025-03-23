9f3c83c404dbbe30aa6eb7920f925f2d9efdd150
NeebinNodin <164468785+NeebinNodin@users.noreply.github.com>
2025-03-22 21:31:41 -0500

    had to redo somethings and use a different computer. got part 1 to get question

--- site.js
+++ site.js
@@ -1,22 +1,29 @@
 
 // import the utility functions "decodeHtml" and "shuffle"
-import { decodeHtml, shuffle } from './utils.js' 
+import { decodeHtml, shuffle } from './utils.js'
 
 // get the elements from the DOM
-const questionElement = document.querySelector('#question')
-const answersElement = document.querySelector('#answers')
-const nextQuestionElement = document.querySelector('#nextQuestion')
+////const questionElement = document.querySelector('#question')
+//const answersElement = document.querySelector('#answers')
+//const nextQuestionElement = document.querySelector('#nextQuestion')
 
-// IIFE (so we can use async/await)
-;(async () => {
+	// IIFE (so we can use async/await)
+	; (async () => {
 
-	// todo: create your "getNextQuestion" function
+		// todo: create your "getNextQuestion" function
+		const getNextQuestion = async () => {
+			const response = await fetch('https://opentdb.com/api.php?amount=1&type=multiple')
+			const json = await response.json()
+			const { question, correct_answer: correct, incorrect_answers: incorrect } = json.results[0]
+			const answers = shuffle([...incorrect, correct])
+			return { question, answers, correct }
 
-	// todo: create your "renderQuestion" function
+		}
+		// todo: create your "renderQuestion" function
 
-	// todo: add the event listener to the "nextQuestion" button
-
-})()
+		// todo: add the event listener to the "nextQuestion" button
+		console.log(await getNextQuestion())
+	})()
 
 // mimic a click on the "nextQuestion" button to show the first question
-nextQuestionElement.click()
+//nextQuestionElement.click()
